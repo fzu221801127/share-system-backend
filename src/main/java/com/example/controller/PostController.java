d6994e26558545041a087b6d417f78513f300de9
fzu221801127 <1647688598@qq.com>
2022-03-03 18:26:22 +0800

    文章列表接口

--- src/main/java/com/example/controller/PostController.java
+++ src/main/java/com/example/controller/PostController.java
@@ -1,9 +1,18 @@
 package com.example.controller;
 
 
+import com.example.entity.Post;
+import com.example.entity.Student;
+import com.example.service.PostService;
+import com.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +22,26 @@ import org.springframework.stereotype.Controller;
  * @author admin
  * @since 2022-03-03
  */
-@Controller
-@RequestMapping("//post")
+@RestController
+@RequestMapping("/v1/posts")
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    /**
+     * 描述:获取全部文章列表
+     * 参数:无
+     * 返回值:文章列表
+     * @author zhuangweilong
+     * @since
+     * @return
+     */
+    @GetMapping("")
+    public List<Post> postList(){
+        return this.postService.list();
+    }
+
 
 }
 
