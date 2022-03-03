a4b47a6af63d24706c35049462d9b8fbd3fcd2bb
fzu221801127 <1647688598@qq.com>
2022-03-03 22:32:55 +0800

    PostController新增部分接口

--- src/main/java/com/example/controller/PostController.java
+++ src/main/java/com/example/controller/PostController.java
@@ -1,16 +1,15 @@
 package com.example.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.entity.Post;
 import com.example.entity.Student;
 import com.example.service.PostService;
 import com.example.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -30,11 +29,47 @@ public class PostController {
     private PostService postService;
 
     /**
+     * 描述:插入新的文章
+     * 参数:文章对象
+     * 返回值:插入成功为true,失败为false
+     * @author zhuangweilong
+     * @since 2022-03-03
+     */
+    @PostMapping("")
+    public boolean insertPost(@RequestBody Post post) {
+        return  this.postService.save(post);
+    }
+
+    /**
+     * 描述:通过id删除文章
+     * 参数:id
+     * 返回值:删除成功为true,失败为false
+     * @author zhuangweilong
+     * @since 2022-03-03
+     */
+    @DeleteMapping ("/id")
+    public boolean deletePostById(Integer id) {
+        return this.postService.removeById(id);
+    }
+
+    /**
+     * 描述:更新文章
+     * 参数:文章对象
+     * 返回值:更新成功为true,失败为false
+     * @author zhuangweilong
+     * @since 2022-03-04
+     */
+    @PutMapping("/id")
+    public boolean update(@RequestBody Post post){
+        return this.postService.updateById(post);
+    }
+
+    /**
      * 描述:获取全部文章列表
      * 参数:无
      * 返回值:文章列表
      * @author zhuangweilong
-     * @since
+     * @since 2022-03-04
      * @return
      */
     @GetMapping("")
@@ -42,6 +77,34 @@ public class PostController {
         return this.postService.list();
     }
 
+    /**
+     * 描述:通过标题模糊搜索文章
+     * 参数:文章标题
+     * 返回值:文章列表
+     * @author zhuangweilong
+     * @since 2022-03-04
+     */
+    @GetMapping("/title")
+    public List<Post> getPostListByName(Post post) {
+        QueryWrapper<Post> queryWrapper = new QueryWrapper<>();
+        queryWrapper.and(
+                wrapper ->
+                        wrapper.like("title", post.getTitle())
+        );
+        return  this.postService.list(queryWrapper);
+    }
+
+    /**
+     * 描述:通过id搜索文章
+     * 参数:文章id
+     * 返回值:文章
+     * @author zhuangweilong
+     * @since 2022-03-04
+     */
+    @GetMapping("/id")
+    public Post getPostById(Post post) {
+        return this.postService.getById(post.getId());
+    }
 
 }
 
