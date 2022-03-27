e6901000e25956b425a06c54e2c50ccf2ad45b4c
fzu221801127 <1647688598@qq.com>
2022-03-27 20:19:05 +0800

    新爬虫接口

--- src/main/java/com/example/controller/PostController.java
+++ src/main/java/com/example/controller/PostController.java
@@ -268,6 +268,19 @@ public class PostController {
         return message;
     }
 
+    @GetMapping("/spider2")
+    public String spider2 (Integer type,Integer firstPage,Integer lastPage) throws IOException {
+        Integer count = 0;
+        String baseurl = "https://www.ygdy8.net";
+        for (int i = firstPage; i<=lastPage; i++) {
+            String number = String.valueOf(i);
+            String url = "https://www.ygdy8.net/html/gndy/oumei/list_"+type.toString()+"_"+number+".html";
+            count += spiderOnePage2(baseurl, url);
+        }
+        String message = "成功爬取"+count+"条信息";
+        return message;
+    }
+
     @GetMapping("/spider/one")
     public Integer spiderOnePage(String baseurl, String url) throws IOException {
         Integer count = 0;
@@ -313,9 +326,55 @@ public class PostController {
         return count;
     }
 
+    @GetMapping("/spider/one2")
+    public Integer spiderOnePage2(String baseurl, String url) throws IOException {
+        System.out.println("********************************************"+url+"!!!!!!!!!!!!!!!!!!!");
+        Integer count = 0;
+        String output = "";
+        String content = HttpClientDownPage.sendGet(url);
+        if (content != null) {
+            Document doc = Jsoup.parse(content);
+            //#header > div > div.bd2 > div.bd3 > div.bd3r > div.co_area2 > div.co_content8 > ul > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(2) > b > a
+            //#header > div > div.bd2 > div.bd3 > div.bd3r > div.co_area2 > div.co_content8 > ul > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > b > a
+            //#header > div > div.bd2 > div.bd3 > div.bd3r > div.co_area2 > div.co_content8 > ul > table:nth-child(25) > tbody > tr:nth-child(2) > td:nth-child(2) > b > a
+            //#header > div > div.bd2 > div.bd3 > div.bd3r > div.co_area2 > div.co_content8 > ul > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > b > a
+            for (int i = 1; i<=25; i++) {
+                String number = String.valueOf(i);
+                Elements elements = doc.select("#header")
+                        .select("div")
+                        .select("div.bd2")
+                        .select("div.bd3")
+                        .select("div.bd3r")
+                        .select("div.co_area2")
+                        .select("div.co_content8")
+                        .select("ul")
+                        .select("table:nth-child("+number+")")
+                        .select("tbody")
+                        .select("tr:nth-child(2)")
+                        .select("td:nth-child(2)")
+                        .select("b")
+                        .select("a");
+                String link = elements.attr("href");
+                String str = elements.toString();
+                String movieName = elements.text();
+                output = output + str + "\n";
+                boolean success = spiderDetail2(baseurl+link,"src/detail.html",movieName);
+                if (success) {
+                    count++;
+                }
+            }
+            FileWriter fileWriter = new FileWriter("src/moviefile.html",true);
+            fileWriter.append(output);
+            fileWriter.flush();
+            fileWriter.close();
+            return count;
+        }
+        return count;
+    }
+
     public boolean spiderDetail (String url, String file,String movieName) throws IOException {
         Post post = new Post();
-        post.setReleasetime("2022-3-19 15:30:31");
+        post.setReleasetime("2022-03-20 15:30:31");
         post.setType("官方资源");
         post.setClick(0);
         post.setUserId("admin");
@@ -338,8 +397,64 @@ public class PostController {
                 if (elements2 != null) {
                     String str2 = elements2.text();
                     if (str2 != null) {
-                        String[] split = str2.split(" ");
-                        String title = split[1];
+                        String title = str2;
+                        System.out.println("$$$$$$$$$$$$$$$$$$$"+title+"%%%%%%%%%%%%%%");
+                        if (movieName != null && movieName != "")
+                        {
+                            post.setTitle(movieName);
+                        } else {
+                            post.setTitle(title);
+                        }
+//                        output = output + title + "\n";
+                    }
+                }
+                String str1 = elements1.toString();
+                if (str1 != null) {
+                    post.setContent(str1);
+//                    output = output + str1 + "\n";
+                }
+            }
+        }
+        if (post != null) {
+            if (post.getTitle() != "" && post.getContent() != ""){
+                System.out.println(post.getTitle()+"\n"+post.getContent());
+                return postService.save(post);
+            }
+        }
+        return false;
+//        FileWriter fileWriter = new FileWriter(file,true);
+//        fileWriter.append(output);
+//        fileWriter.flush();
+//        fileWriter.close();
+    }
+
+    public boolean spiderDetail2 (String url, String file,String movieName) throws IOException {
+        Post post = new Post();
+        post.setReleasetime("2022-03-20 15:30:31");
+        post.setType("官方资源");
+        post.setClick(0);
+        post.setUserId("admin");
+        post.setState("未被举报");
+        post.setTitle("");
+        post.setContent("");
+
+        String output = "";
+        String content = HttpClientDownPage.sendGet(url);
+        if (content != null) {
+            Document doc = Jsoup.parse(content);
+            //#Zoom > span > a
+            //#Zoom > span > font:nth-child(31) > a
+            //#Zoom > span
+
+            Elements elements1 = doc.select("#Zoom")
+                    .select("span");
+            if (elements1 != null) {
+                Elements elements2 = elements1.select("a");
+                if (elements2 != null) {
+                    String str2 = elements2.text();
+                    if (str2 != null) {
+                        String title = str2;
+                        System.out.println("$$$$$$$$$$$$$$$$$$$"+title+"%%%%%%%%%%%%");
                         if (movieName != null && movieName != "")
                         {
                             post.setTitle(movieName);
